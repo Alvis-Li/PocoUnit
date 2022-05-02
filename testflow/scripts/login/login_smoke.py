f7183e31ed1dcc7f1d4d34cef0a96a09d7004f7d
alvis <levy@levyleo.cn>
2022-05-02 14:10:38 +0800

    update

--- testflow/scripts/login/login_smoke.py
+++ testflow/scripts/login/login_smoke.py
@@ -5,8 +5,8 @@ def runTest(self, poco):
     poco("com.fanmao.bookkeeping:id/rela_tab_4").wait(3).click()
     poco("com.fanmao.bookkeeping:id/img_my_header").click()
     wait(Template(r"res/img/logout.png", record_pos=(0.003, -0.504), resolution=(1040, 1664)))
-    poco("com.fanmao.bookkeeping:id/et_mobile").set_text('18601687641')
-    poco("com.fanmao.bookkeeping:id/et_password").set_text('123456')
+    poco("com.fanmao.bookkeeping:id/et_mobile").set_text(os.getenv('et_mobile'))
+    poco("com.fanmao.bookkeeping:id/et_password").set_text(os.getenv('et_password'))
     poco("com.fanmao.bookkeeping:id/btn_login").click()
     assert_equal(poco("com.fanmao.bookkeeping:id/tv_my_name").wait(3).get_text(), 'null')
 
