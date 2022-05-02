7ba1a36b6dfc61b8615d87af2f9d9cd80314132f
alvis.li <alvis>
2022-05-02 13:07:52 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PocoUnit
+
+## 安装依赖包
+```
+pip install -r requirements.txt -i  https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+## 环境变量
+将需要的环境变量放入`.env`文件中，runner.py会通过`python-dotenv`来读取。
--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ import argparse
 import pocounit
 import importlib.util
 
+from dotenv import load_dotenv
 from pocounit.case import PocoTestCase
 from pocounit.suite import PocoTestSuite
 from pocounit.addons.poco.capturing import SiteCaptor
@@ -12,16 +13,24 @@ from pocounit.addons.poco.action_tracking import ActionTracker
 from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 from airtest.core.api import connect_device, device as current_device, start_app, stop_app, uninstall
 
-package_name = "com.xmiles.jdd"
-device_uri = "Android:///emulator-5554"
-project_root = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), '../automation')
-libs_root = os.path.join(project_root, "testflow", "libs")
-test_case_dir = os.path.join(project_root, "testflow", "scripts")
-run_test_label = ""
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--smoke', help='执行冒烟测试用例', action='store_true')
+parser.add_argument('-f', '--file', help='指定执行文件, 如: test/test.py')
+args = parser.parse_args()
 
-os.system("adb kill-server")  # 重启adb
-sys.path.insert(0, os.path.join(project_root, "testflow"))
+load_dotenv()
+package_name = os.getenv('package_name')
+device_uri = os.getenv('device_uri')
+project_root = os.path.dirname(os.path.abspath(__file__))
+libs_root = os.path.join(project_root, os.getenv(
+    'src_dir', 'testflow'), os.getenv('libs_dir', 'libs'))
+test_case_dir = os.path.join(project_root, os.getenv(
+    'src_dir', 'testflow'), os.getenv('scripts_dir', 'scripts'))
+
+os.system('adb kill-server')  # 重启adb
+sys.path.insert(0, os.path.join(
+    project_root, os.getenv('src_dir', 'testflow')))
+
 
 class AndroidAppCase(PocoTestCase):
     @classmethod
@@ -44,7 +53,7 @@ class AndroidAppSuite(PocoTestSuite):
             connect_device(device_uri)
 
         dev = current_device()
-        # 去掉录制
+        # 去掉录制。如果需要录制，注释掉下面三行代码
         dev.recorder.start_recording = lambda max_time, bit_rate: self
         dev.recorder.stop_recording = lambda output, is_interrupted: self
         dev.recorder.pull_last_recording_file = lambda output: self
@@ -52,10 +61,9 @@ class AndroidAppSuite(PocoTestSuite):
         start_app(package_name)
 
     def tearDown(self):
-        pass
         stop_app(package_name)
-        uninstall("com.netease.nie.yosemite")
-        uninstall("com.netease.open.pocoservice")
+        uninstall('com.netease.nie.yosemite')
+        uninstall('com.netease.open.pocoservice')
 
 
 def moduleFromFile(module_name, file_path):
@@ -67,32 +75,38 @@ def moduleFromFile(module_name, file_path):
 
 def makeNewClass(x, file):
     dict = {}
-    if hasattr(file, "runTest"):
-        dict["runTest"] = lambda self: file.runTest(self, self.poco)
-    if hasattr(file, "setUp"):
-        dict["setUp"] = lambda self: file.setUp(self, self.poco)
-    if hasattr(file, "tearDown"):
-        dict["tearDown"] = lambda self: file.tearDown(self, self.poco)
+    if hasattr(file, 'runTest'):
+        dict['runTest'] = lambda self: file.runTest(self, self.poco)
+    if hasattr(file, 'setUp'):
+        dict['setUp'] = lambda self: file.setUp(self, self.poco)
+    if hasattr(file, 'tearDown'):
+        dict['tearDown'] = lambda self: file.tearDown(self, self.poco)
 
     new_class = type(x, (AndroidAppCase,), dict)
 
-    if hasattr(file, "getMetaInfo"):
-        setattr(new_class, "getMetaInfo", file.getMetaInfo)
-    if hasattr(file, "name"):
-        setattr(new_class, "name", file.name)
+    if hasattr(file, 'getMetaInfo'):
+        setattr(new_class, 'getMetaInfo', file.getMetaInfo)
+    if hasattr(file, 'name'):
+        setattr(new_class, 'name', file.name)
     return new_class
 
 
-if __name__ == "__main__":
-    file_list = glob.glob(test_case_dir + "/**/*.py", recursive=True)
+if __name__ == '__main__':
+    file_list = glob.glob(test_case_dir + '/**/*.py', recursive=True)
     tests = []
     for file_path in file_list:
         file = moduleFromFile(project_root, file_path)
-        test_case_filename, _ = os.path.splitext(os.path.basename(file_path))
-        if len(run_test_label) > 0 and (run_test_label not in test_case_filename):
+        test_case_filename, _ = os.path.splitext(
+            os.path.relpath(file_path, test_case_dir))
+        if args.file and args.file != test_case_filename+'.py':
+            continue
+        if args.smoke and ('smoke' not in test_case_filename):
             continue
         sub_class = makeNewClass(test_case_filename, file)
         tests.append(sub_class())
 
-    suite = AndroidAppSuite(tests)
-    pocounit.run(suite)
+    if len(tests) > 0:
+        suite = AndroidAppSuite(tests)
+        pocounit.run(suite)
+    else:
+        raise Exception('未找到任何测试用例')
